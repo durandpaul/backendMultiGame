b2c78a669e061900c4c81658d8b7777fa562aea0
durand_p <paul.a.durand@outlook.fr>
2019-04-08 19:41:00 +0200

    try 2

---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict';
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors') ;
+const http = require('http').Server(app);
+const port = process.env.PORT || '3000';
+require('./server/db');
+
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
+
+const userRouter = require('./server/routes/userRouter');
+const dataRouter = require('./server/routes/dataRouter');
+const socket = require('./server/lib/socket');
+
+// require('./models/dataGameM');
+// require('./models/roomM');
+// require('./models/userM');
+
+app.use(userRouter);
+app.use(dataRouter);
+
+socket.socketIo(http);
+
+http.listen(port, () => {
+    console.log(`listening on ${port}`);
+});
