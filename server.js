71d4ecf5def5a691d1f2f7c56a2d9145f75cd51b
durand_p <paul.a.durand@outlook.fr>
2019-04-08 23:11:07 +0200

    add static path in server

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
 const userRouter = require('./server/routes/userRouter');
 const dataRouter = require('./server/routes/dataRouter');
 const socket = require('./server/lib/socket');
