b522cad2808eca0fbe4b5753d221639474593f37
durand_p <paul.a.durand@outlook.fr>
2019-04-09 00:38:04 +0200

    server try 7

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname, 'dist'));
+// app.use(express.static(__dirname, 'dist'));
 
 // app.get('*', (req, res) => {
 //   res.sendFile(path.join(__dirname + '/dist/index.html'));
