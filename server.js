45744b56c346a720277825e7fc689e23858cfa15
durand_p <paul.a.durand@outlook.fr>
2019-04-08 23:25:30 +0200

    server try 2

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
 
-// app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname + '/dist/index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 const userRouter = require('./server/routes/userRouter');
 const dataRouter = require('./server/routes/dataRouter');
